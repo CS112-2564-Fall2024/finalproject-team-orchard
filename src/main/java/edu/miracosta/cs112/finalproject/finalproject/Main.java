e14d075a9faa0db5eadc2f2a5d720b9c33eb8ddb
MCCLab4804WD18 <olivierek@gmail.com>
2024-11-27 13:49:50 -0800

    11/27/24 Oliver Trojanowski

--- src/main/java/edu/miracosta/cs112/finalproject/finalproject/Main.java
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/Main.java
@@ -12,18 +12,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            // Load the FXML file
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/edu/miracosta/cs112/finalproject/finalproject/Calendar.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Calendar.fxml"));
             AnchorPane root = loader.load();
-
-            // Set the title of the stage (window)
             primaryStage.setTitle("Calendar Application");
-
-            // Create the scene with the loaded FXML and set it on the stage
             Scene scene = new Scene(root, 1050, 850);
             primaryStage.setScene(scene);
-
-            // Show the stage
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
@@ -32,7 +25,6 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        // Launch the application
         launch(args);
     }
 }
